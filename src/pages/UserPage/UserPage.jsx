29f96fa7fe62a1326c73b3ffdbaad0d6ec7dd57c
AlbertMkrtchyanJs <albert.mkrtchyan2008@gmail.com>
2025-03-27 20:40:16 +0400

    commit3

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import User from "../../components/User/User";
+import style from './UserPage.module.css'
 
 const UserPage = () => {
   const { users } = useSelector((state) => state.getUserState);
 
   return (
-    <div>
+    <div className={style.b}>
       {users.map((user) => {
         return <User key={user.id} user={user} />;
       })}
