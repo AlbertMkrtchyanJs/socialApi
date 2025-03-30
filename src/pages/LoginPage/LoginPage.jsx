956a2cbc8759f55aba6cd3f806bbecceb3be5220
AlbertMkrtchyanJs <albert.mkrtchyan2008@gmail.com>
2025-03-30 21:00:03 +0400

    commit6

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,9 +2,8 @@ import { Formik, Form, Field } from "formik";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loginTC } from "../../store/reducers/authreducer/authReducer";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import validation from "../../schema/schema";
-
 
 const LoginPage = () => {
   const dispatch = useDispatch();
@@ -32,9 +31,7 @@ const LoginPage = () => {
         <Form>
           <Field name="email" placeholder="Email" />
           <Field type="password" name="password" placeholder="Password" />
-          <NavLink>
-            <button type="submit">Log In</button>
-          </NavLink>
+          <button type="submit">Log In</button>
         </Form>
       </Formik>
     </div>
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,47 +1,32 @@
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
-import { getprofileThunk } from '../../store/reducers/profileReducer'
-
-import style from './ProfilePage.module.css'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { profileTC } from "../../store/reducers/profileReducer/profileReducer";
+import gago from "../../assets/gago.jpg";
 
 const ProfilePage = () => {
-  const { id } = useParams()
-  const dispatch = useDispatch()
-  const { profile, loading } = useSelector(store => store.profileReducer)
-
-  const { fetching } = useSelector(store => store.usersReducer)
-
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { profile } = useSelector((store) => store.profileState);
 
   useEffect(() => {
     if (id) {
       if (!profile || profile.userId !== Number(id)) {
-        dispatch(getprofileThunk(id))
+        dispatch(profileTC(id));
       }
     }
-  }, [id, profile, dispatch])
-
-  if (fetching || loading) {
-    return <div className={style.loading}>Загрузка...</div>
-  }
+  }, [id, profile, dispatch]);
 
   return (
-    <div className={style.profileContainer}>
-      <div className={style.profileCard}>
-        <div className={style.profileHeader}>
-          <h1 className={style.profileName}>{profile.fullName}</h1>
+   
+      <>
+        <h1>{profile.fullName}</h1>
+        <div>
+          <img src={profile.photos || gago} />
         </div>
-        <div className={style.profileContent}>
-          <div className={style.profileImageContainer}>
-            <img
-              className={style.profileImage}
-              src={profile.photos.large || "https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_640.png"}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+      </>
+   
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
